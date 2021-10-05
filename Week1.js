6e246b4372c6e0543a2665e4e0b0b7e0605e7660
StuartL457 <stuart.lewis95@gmail.com>
2021-10-05 14:18:17 +0100

    1 file added, 1 deleted

---
+++ Week1.js
@@ -0,0 +1,7 @@
+// run `node Week1.js` in the terminal
+function print(value){
+  console.log(value)
+}
+
+print(`Hello Node.js v${process.versions.node}!`);
+
--- index.js
+++
@@ -1,7 +0,0 @@
-// run `node index.js` in the terminal
-function print(value){
-  console.log(value)
-}
-
-console.log(`Hello Node.js v${process.versions.node}!`);
-
