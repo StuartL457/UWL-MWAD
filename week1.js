3757848fe920f63842e17319c253537a43825bab
StuartL457 <stuart.lewis95@gmail.com>
2021-10-05 14:34:20 +0100

    2 files added, 1 deleted

--- week1.js
+++ week1.js
@@ -1,14 +1,12 @@
 // run `node Week1.js` in the terminal
 
 //slide 5
-var value = typeof 4.5;
-print(value);
-value = typeof 'x';
-print(value);
+
+print(typeof 4.5);
+print(typeof 'x');
 
 //slide 6
-value = Math.max(3, 5);
-print(value);
+print(Math.max(3, 5));
 
 //slide 7
 print(3 > 2);
